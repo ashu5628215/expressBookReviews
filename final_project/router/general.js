a7516c87b296c7646100d3e51c8a9f58ae927379
ashu5628215 <mnnitashu5628215@gmail.com>
2024-08-30 11:43:16 -0400

    task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,8 +67,12 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const reqIsbn = req.params.isbn;
+    if (books[reqIsbn]) {
+        res.send(books[reqIsbn].reviews);
+    } else {
+        res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.general = public_users;
